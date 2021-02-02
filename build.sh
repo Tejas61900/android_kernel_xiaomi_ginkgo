04e5892aec382bcc6c96abe238ff8e2bc397d4f7
HafizZiq <hafizuddinismail552@gmail.com>
2021-02-02 23:30:53 +0800

    PerawanX: Its me boi defconfig: keep da fookin file name,so i can fuck with history
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- build.sh
+++ build.sh
@@ -1,33 +1,12 @@
 echo -e "\nStarting compilation...\n"
 # ENV
-R=n
-while read -p "You want to build for MIUI/Q or OOS? (q/oos) " bchoice; do
-case "$bchoice" in
- q|Q)
-  S="Q"
-  break
- ;;
- oos|OOS)
-  S="OOS"
-  R=y
-  export CONFIG_THINLTO=y
-  git cp -s 3679d8fbfbf11151109c71eb4308a21d4fb854ab
-  break
- ;;
- *)
-  echo
-  echo "Try again please!"
-  echo
- ;;
-esac
-done
 CONFIG=vendor/sixteen_defconfig
 KERNEL_DIR=$(pwd)
 PARENT_DIR="$(dirname "$KERNEL_DIR")"
 KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
 export KBUILD_BUILD_USER="hafizziq"
 export KBUILD_BUILD_HOST="ubuntu"
-# export KBUILD_BUILD_VERSION="69"
+export KBUILD_BUILD_VERSION="69"
 export KBUILD_BUILD_TIMESTAMP="$(TZ=Asia/Kuala_Lumpur date)"
 export PATH="$HOME/clang/proton/bin:$PATH"
 export LD_LIBRARY_PATH="$HOME/clang/proton/lib:$LD_LIBRARY_PATH"
@@ -63,13 +42,12 @@ clang_build () {
 # Build kernel
 make O=$out ARCH=arm64 $CONFIG > /dev/null
 echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
-echo -e "\nCompiling $ZIPNAME\n"
 clang_build
 if [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "$out/arch/arm64/boot/dtbo.img" ]; then
  echo -e "\nKernel compiled succesfully! Zipping up...\n"
- ZIPNAME="SixTeen•Kernel•"$S"•Ginklow-$(date '+%Y%m%d-%H%M').zip"
+ ZIPNAME="PerawanX☭•Q•Ginklow-$(date '+%Y%m%d-%H%M').zip"
  if [ ! -d AnyKernel3 ]; then
-  git clone -q https://github.com/HafizZiq/AnyKernel3 -b sixteen
+  git clone -q https://github.com/HafizZiq/AnyKernel3 -b perawanx
  fi;
  mv -f $out/arch/arm64/boot/Image.gz-dtb AnyKernel3
  mv -f $out/arch/arm64/boot/dtbo.img AnyKernel3
@@ -78,12 +56,9 @@ if [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "$out/arch/arm64/boot/dtbo
  cd ..
  rm -rf AnyKernel3
  echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
- echo -e "Zip: $ZIPNAME\n"
+ echo "Zip: $ZIPNAME"
  rm -rf $out
 else
- echo -e "\nCompilation failed!\n"
-fi;
-if [ $R == y ]; then
- git rs --hard $(git log --pretty=oneline|head -n2|tail -n1|awk '{ print $1}')
+ echo -e "\nCompilation failed!"
 fi;
 exit 0
